25999b0cf8c23dc21cb5b541aacd2ae47987e5c0
manojkumarp <manojkumarp@buddi.ai>
2021-12-23 22:08:02 +0530

    added

---
+++ combinationSum-2.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        self.backtrack(candidates,ans,target,[])
+        return(ans)
+    def backtrack(self,candy,ans,target,temp):
+        if(target==0):
+            ans.ext
+x=Solution()
+arr=[2,7,6,3,5,1]
+target=9
+anss=x.combinationSum(arr,target)
+print(anss)
+sam=[[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,2],[1,1,1,1,1,1,3],[1,1,1,1,1,2,2],[1,1,1,1,2,3],[1,1,1,1,5],[1,1,1,2,2,2],[1,1,1,3,3],[1,1,1,6],[1,1,2,2,3],[1,1,2,5],[1,1,7],[1,2,2,2,2],[1,2,3,3],[1,2,6],[1,3,5],[2,2,2,3],[2,2,5],[2,7],[3,3,3],[3,6]]
+print(sam.sort()==anss.sort())

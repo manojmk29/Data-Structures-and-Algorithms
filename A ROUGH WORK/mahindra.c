568d871baa9eaa513051c9763ac79eef623207a1
manojkumarp <manojkumarp@buddi.ai>
2022-09-23 23:24:02 +0530

    ADDED

---
+++ A ROUGH WORK/mahindra.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+int discountAMT(int orderValue){
+    int answer;
+    
+    return answer;
+}
+
+int main()
+{
+    int orderValue;
+    scanf("%d",&orderValue);
+    int result=discountAMT(orderValue);
+    printf("%d",result);
+    return 0;
+}
